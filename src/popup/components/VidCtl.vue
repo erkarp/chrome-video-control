3b221bfabf5081ad366ffeaff66b5897f5543e64
ekarp <emilyrkarp@gmail.com>
2018-10-28 00:49:52 -0400

    go to tab button

--- src/popup/components/VidCtl.vue
+++ src/popup/components/VidCtl.vue
@@ -5,6 +5,7 @@
       button(v-on:click='play') play
       button(v-on:click='pause') pause
       button(v-on:click='rewind') -10
+      button(v-on:click='highlight') tab
       button(v-on:click='close') X
 </template>
 
@@ -28,6 +29,10 @@
         chrome.tabs.executeScript(this.video.id, {
           code: 'document.getElementsByTagName("video").item(0).currentTime -= 10'
         })
+      },
+      highlight: function () {
+        console.log(this.video)
+        chrome.tabs.highlight({tabs: this.video.index})
       },
       close: function () {
         chrome.tabs.remove(this.video.id)
